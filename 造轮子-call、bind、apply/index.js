06f9e493e50c4bfab79b516204985a8c9b80ee0a
zhengzy <2584007592@qq.com>
2021-06-24 14:56:46 +0800

    call bind apply

---
+++ 造轮子-call、bind、apply/index.js
@@ -0,0 +1,32 @@
+Function.prototype.myCall = function (_this = window, ...arguments) {//展开剩余参数   _this没传默认值为window
+  // 将执行函数的this赋值给传进来的值
+  _this.myFunc = this
+  // 执行函数并存下返回值
+  const result = _this.myFunc(...arguments)
+  // 添加的函数置为null，可以让浏览器进行回收
+  _this.myFunc = null
+  return result
+}
+
+Function.prototype.myApply = function (_this = window, arr) {// _this没传默认值为window
+  // 将执行函数的this赋值给传进来的值
+  _this.myFunc = this
+  // 执行函数并存下返回值
+  const result = _this.myFunc(...arr)
+  // 添加的函数置为null，可以让浏览器进行回收
+  _this.myFunc = null
+  return result
+}
+
+Function.prototype.myBind = function (_this = window, arr) {// _this没传默认值为window
+  // 将执行函数的this赋值给传进来的值
+  _this.myFunc = this
+  // 执行函数并存下返回值
+  const result = function () {
+    _this.myFunc(...arr)
+    // 添加的函数置为null，可以让浏览器进行回收
+    _this.myFunc = null
+  }
+
+  return result
+}
