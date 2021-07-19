13cd86b22a5415156be0d26946ffbaea1fea9dfe
zhengzy <2584007592@qq.com>
2021-07-19 15:18:20 +0800

    add懒加载

--- 造轮子-节流/index.js
+++ 造轮子-节流/index.js
@@ -1,11 +1,11 @@
-function $(e) {
+function $ (e) {
   return document.querySelector(e)
 }
 let flag = false,
   timer = null
 
 //节流原理：在一定时间内，只能触发一次
-function throttle(func, wait = 1500) {
+function throttle (func, wait = 1500) {
   if (!flag) {
     flag = true
     typeof func === 'function' && func()
@@ -14,3 +14,4 @@ function throttle(func, wait = 1500) {
     }, wait)
   }
 }
+
