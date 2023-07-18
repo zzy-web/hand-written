9aa1b21c974a9acbda02eeefbf9c5229b34aa39d
zhengzy <53005627+zzy-web@users.noreply.github.com>
2023-07-18 15:55:58 +0800

    Update index.js

--- 造轮子-call、bind、apply/index.js
+++ 造轮子-call、bind、apply/index.js
@@ -8,7 +8,7 @@ Function.prototype.myCall = function (_this = window, ...arguments) {//展开剩
   return result
 }
 
-Function.prototype.myApply = function (_this = window, arr) {// _this没传默认值为window
+Function.prototype.myApply = function (_this = window, arr = []) {// _this没传默认值为window
   // 将执行函数的this赋值给传进来的值
   _this.myFunc = this
   // 执行函数并存下返回值
